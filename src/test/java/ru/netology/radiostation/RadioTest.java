abf840ef33fa50121c1258c0b78eeb9ecc1dd7b2
Evgesha <Slav.novikova2014@gmail.com>
2023-06-09 18:26:57 +0700

    change RadioTest

--- src/test/java/ru/netology/radiostation/RadioTest.java
+++ src/test/java/ru/netology/radiostation/RadioTest.java
@@ -5,6 +5,14 @@ import org.junit.jupiter.api.Test;
 
 public class RadioTest {
     @Test
+    public void test() {
+        Radio cond = new Radio(40);
+        Assertions.assertEquals(0, cond.getMinRadiostation());
+        Assertions.assertEquals(40, cond.getMaxRadiostation());
+        Assertions.assertEquals(0, cond.getCurrentRadiostation());
+    }
+
+    @Test
     public void shouldSetRadiostation() {
         Radio cond = new Radio();
         cond.setCurrentRadiostation(9);
@@ -13,7 +21,6 @@ public class RadioTest {
         Assertions.assertEquals(expected, actual);
     }
 
-    
     @Test
     public void shouldSetVolume() {
         Radio cond = new Radio();
@@ -31,14 +38,16 @@ public class RadioTest {
         int actual = cond.getCurrentRadiostation();
         Assertions.assertEquals(expected, actual);
     }
+
     @Test
-    public void radiostationNotThenBelowMin(){
+    public void radiostationNotThenBelowMin() {
         Radio cond = new Radio();
         cond.setCurrentRadiostation(-5);
         int expected = 0;
         int actual = cond.getCurrentRadiostation();
         Assertions.assertEquals(expected, actual);
     }
+
     @Test
     public void VolumeNotThenBelowMin() {
         Radio cond = new Radio();
@@ -47,6 +56,7 @@ public class RadioTest {
         int actual = cond.getCurrentVolume();
         Assertions.assertEquals(expected, actual);
     }
+
     @Test
     public void volumeNotMoreThenAboveMax() {
         Radio cond = new Radio();
@@ -105,8 +115,8 @@ public class RadioTest {
     @Test
     public void maxRadiostationAfterUseNext() {
         Radio cond = new Radio();
-        cond.setCurrentRadiostation(9);
-        int expected = 0;
+        cond.setCurrentRadiostation(cond.getMaxRadiostation());
+        int expected = cond.getMinRadiostation();
         int actual = cond.nextRadiostation();
         Assertions.assertEquals(expected, actual);
     }
@@ -123,8 +133,8 @@ public class RadioTest {
     @Test
     public void minRadiostationAfterUsePrev() {
         Radio cond = new Radio();
-        cond.setCurrentRadiostation(0);
-        int expected = 9;
+        cond.setCurrentRadiostation(cond.getMinRadiostation());
+        int expected = cond.getMaxRadiostation();
         int actual = cond.prevRadiostation();
         Assertions.assertEquals(expected, actual);
     }
